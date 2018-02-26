08dea75adcddcdd74ab277a80ac0510c33c7525a
lalfaro1704 <luipi.alfaro@gmail.com>
2018-02-26 18:07:46 -0300

    Adding filters

--- django_dynamic_forms/admin.py
+++ django_dynamic_forms/admin.py
@@ -2,20 +2,29 @@
 from django.contrib import admin
 
 # Imports from project
-from .models import (DynamicForm, DynamicAttribute)
+from .models import (DynamicForm, DynamicAttribute, ValueAttribute)
 
 
 class DynamicAttributeAdmin(admin.ModelAdmin):
     model = DynamicAttribute
     icon = '<i class="material-icons">input</i>'
     list_display = ('name', 'field_type', )
+    list_filter = ['name', 'field_type']
 
 
 class DynamicFormAdmin(admin.ModelAdmin):
     model = DynamicForm
     icon = '<i class="material-icons">reorder</i>'
     list_display = ('name', 'parent', )
+    list_filter = ['name']
+
+
+class ValueAttributeAdmin(admin.ModelAdmin):
+    model = ValueAttribute
+    icon = '<i class="material-icons">reorder</i>'
+    list_display = ('name', 'parent', )
 
 
 admin.site.register(DynamicAttribute, DynamicAttributeAdmin)
 admin.site.register(DynamicForm, DynamicFormAdmin)
+admin.site.register(ValueAttribute, ValueAttributeAdmin)
