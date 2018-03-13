f2750c044eefc3666595d1061ec15f00ee8d1d39
lalfaro1704 <luipi.alfaro@gmail.com>
2018-03-13 18:13:51 -0300

    translate for app name

--- django_dynamic_forms/apps.py
+++ django_dynamic_forms/apps.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class Config(AppConfig):
-    name = 'django_dynamic_forms'
+    name = _('django_dynamic_forms')
 
