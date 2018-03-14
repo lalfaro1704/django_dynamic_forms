f88cefbe326c9bcb6e800586242687e233d8dec7
lalfaro1704 <luipi.alfaro@gmail.com>
2018-03-14 12:24:45 -0300

    translate for app name

--- django_dynamic_forms/apps.py
+++ django_dynamic_forms/apps.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class DjangoDynamicFormsConfig(AppConfig):
     name = 'django_dynamic_forms'
     icon = '<i class="material-icons">memory</i>'
     verbose_name = _('Django Dynamic Forms')
-
