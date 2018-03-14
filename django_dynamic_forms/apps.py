d3e653a172c1b9a88ad328dde0aec1b20680353f
lalfaro1704 <luipi.alfaro@gmail.com>
2018-03-14 12:23:37 -0300

    translate for app name

--- django_dynamic_forms/apps.py
+++ django_dynamic_forms/apps.py
@@ -2,6 +2,8 @@
 from django.apps import AppConfig
 
 
-class Config(AppConfig):
+class DjangoDynamicFormsConfig(AppConfig):
     name = 'django_dynamic_forms'
+    icon = '<i class="material-icons">memory</i>'
+    verbose_name = _('Django Dynamic Forms')
 
