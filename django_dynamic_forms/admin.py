e0994022c49f2786ad3131ee187eb2532339a5f9
lalfaro1704 <luipi.alfaro@gmail.com>
2018-05-18 15:56:35 -0400

    generador de informes

--- django_dynamic_forms/admin.py
+++ django_dynamic_forms/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 # Imports from project
 from .models import (DynamicForm, DynamicAttribute, FormAttribute,
                      SimpleOptionSelects, DynamicParameter, ListName,
-                     ListOptionSelect)
+                     ListOptionSelect, ReportDetail, ReportSection)
 
 
 class DynamicAttributeAdmin(admin.ModelAdmin):
